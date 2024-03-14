2ac810fd31ba90ddda77a36ec4e19007c121e61a
Александр <44487342+OyasumiiAlex@users.noreply.github.com>
2024-03-13 21:44:57 -0600

    Update arraystring.sql
    fixing syntax

--- DB_Ejercito/functions/arraystring.sql
+++ DB_Ejercito/functions/arraystring.sql
@@ -4,10 +4,10 @@ DECLARE
 	i record;
 	c int:=1;
 BEGIN
-	for i in select *from soldado;
+	for i in select *from soldados
 		loop
-			matriz[c]:=i.nomb_sol;
-			raise notice 'El nombre del soldado es %', matriz[[c];
+			matriz[c]:=i.nombre_sol;
+			raise notice 'El nombre del soldado es %', matriz[c];
 			c:=c+1;
 		end loop;
 END;
