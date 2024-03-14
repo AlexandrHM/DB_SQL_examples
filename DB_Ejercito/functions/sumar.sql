737395b87055213415c0cab3df1cfa9266222f87
Александр <44487342+OyasumiiAlex@users.noreply.github.com>
2024-03-13 22:00:17 -0600

    Update sumar.sql
    fixing logic function

--- DB_Ejercito/functions/sumar.sql
+++ DB_Ejercito/functions/sumar.sql
@@ -1,11 +1,11 @@
 CREATE OR REPLACE FUNCTION suma(int, int) returns int as $$
 declare
 	dato1 int;
-	dato1 int;
+	dato2 int;
 	r int;
 BEGIN
 	dato1:=$1;
-	dato1:=$2;
+	dato2:=$2;
 	r:=dato1+dato2;
 	return r;
 END;
